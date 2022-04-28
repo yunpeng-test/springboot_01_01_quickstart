6dd0dd11075c3256b6df0140b2fe3c960d0b48bc
yunpeng-test <104405229+yunpeng-test@users.noreply.github.com>
2022-04-28 14:47:19 +0800

    Update BookController.java

--- src/main/java/com/itheima/controller/BookController.java
+++ src/main/java/com/itheima/controller/BookController.java
@@ -4,13 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
     @GetMapping
     public String getById() {
-        System.out.println("springboot log is running...");
+        Random random = new Random();
+        System.out.println("springboot log is running..." + random.ints());
         return "springboot web is running...";
     }
 }
+
